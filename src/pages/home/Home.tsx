0abab381e29a48effe9a6b98afc86bc5cc9615b3
NandayGB <fernandagbarbosa97@gmail.com>
2024-04-22 10:38:19 -0300

    criação do home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,30 @@
+import './Home.css';
 
 function Home() {
     return (
         <>
-            <div className="bg-purple-500 flex justify-center">
-            <div className="container grid grid-cols-2 text-white">
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">
-                            FÁRMACIA NANDAY
-                        </h2>
-                        <p className='text-xl'>
-                        Aqui você poderá cadastrar seus produtos de forma rápida e prática,
-                         otimizando seu tempo e facilitando o gerenciamento de estoque
-                        </p>
-                    </div>
-
-                    <div className="flex justify-center ">
-                        <img src="https://br.freepik.com/vetores-premium/farmacia-com-enfermeira-no-balcao-personagem-de-desenho-animado-de-farmacia_3693336.htm?epik=dj0yJnU9TjN5Sm1sREZPdTZzbnllSENIZGZtS3U4MDJqS3FhZWgmcD0wJm49MWF3WnphNVVhLUs0XzhkSy1EYTl2USZ0PUFBQUFBR1ltWGdJ"
-                            alt="Imagem Página Home"
-                            className='w-2/3'
-                        />
-                    </div>
-                </div>
+              <h2 className='text-5xl font-bold'>Seja Bem vindo a Farmácia Nanday!</h2>
+              <p className='text-xl'>Na nossa farmácia online, você encontra tudo o que precisa para cuidar da sua saúde e bem-estar,
+               com conveniência, segurança e qualidade garantida.  </p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Cadastrar Produtos</button>
+              </div>
             </div>
-        </>
-    )
+  
+            <div className="flex justify-center ">
+             
+      
+            </div>
+          </div>
+        </div>
+      
+      </>
+    );
 }
 
-export default Home
+export default Home;
